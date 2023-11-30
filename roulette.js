250fdf824b7632700d2727b868c109df4d153e29
Roxpj <148432952+Roxpj@users.noreply.github.com>
2023-11-30 19:20:39 +0100

    added funtion spinWheel, success!

---
+++ roulette.js
@@ -0,0 +1,7 @@
+function spinWheel(){
+    let ruleta = document.querySelector("#ruleta");
+    ruleta.style.cssText = "-webkit-animation: 3s rotate linear infinite; animation: 3s rotate linear infinite;-webkit-transform-origin: 50% 50%;transform-origin: 50% 50%;"
+}
+
+
+
