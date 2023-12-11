32269a4a46efb9f2101db60f68b9d584ffbaaa9c
Yadyvivian <148440973+Yadyvivian@users.noreply.github.com>
2023-12-11 11:31:40 +0100

    change in js  array participantes

--- participants.js
+++ participants.js
@@ -1,10 +1,15 @@
+var participantes= [] //array para los nombres
+
 function agregarParticipante(event) {
     event.preventDefault(); // Evitar que se envíe el formulario
     
     var nombre = document.getElementById('nombre').value;
   
     if (nombre !== '') {
+      participantes.push(nombre)// mete los nombres en el array
       var lista = document.getElementById('listaParticipantes');
+      sessionStorage.setItem ("participantes", JSON.stringify(participantes)); //guarda el array en el navegador
+      
       var nuevoParticipante = document.createElement('li');
       nuevoParticipante.appendChild(document.createTextNode(nombre));
       
@@ -13,6 +18,8 @@ function agregarParticipante(event) {
       botonEliminar.appendChild(document.createTextNode('X'));
       botonEliminar.onclick = function() {
         lista.removeChild(nuevoParticipante);
+        participantes.splice (participantes.indexOf(nombre), 1); //elimina del array
+        sessionStorage.setItem ("participantes", JSON.stringify(participantes)); //guarda el array en el navegador
       };
       nuevoParticipante.appendChild(botonEliminar);
       (botonEliminar).style.color= '#900C3F';
