fc37161c8b9dbe9ae68f883d5e312dc64691ea5f
Roxpj <148432952+Roxpj@users.noreply.github.com>
2023-12-11 21:28:21 +0100

    added audio to btn in start

--- participants.js
+++ participants.js
@@ -44,7 +44,5 @@ function agregarParticipante(event) {
   function audiobtn(){
     let audioBoton = document.querySelector("#audio-btn");
     let audioPagina = document.querySelector("#audio");
-    audioPagina.pause();
-    audioPagina.currentTime = 0;
     audioBoton.play();
   }
--- start.js
+++ start.js
@@ -1,4 +1,12 @@
 function redirectToOtraPagina() {
+  setTimeout(()=> {
     window.location.href = 'participants.html';
+  }, 1000)
+  }
+
+  function audiobtn(){
+    let audioBoton = document.querySelector("#audio-btn");
+    let audioPagina = document.querySelector("#audio");
+    audioBoton.play();
   }
   
